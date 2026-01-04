613980b92fc60b3bd9f10467e2674320251092e2
Louison HACHEMI <58892702+Layttos@users.noreply.github.com>
2026-01-04 01:54:12 +0100

    Update main.go

--- main.go
+++ main.go
@@ -39,8 +39,8 @@ on is_running(appName)
 	tell application "System Events" to (name of processes) contains appName
 end is_running
 
-set safRunning to is_running("Music")
-if safRunning is true then
+set appRunning to is_running("Music")
+if appRunning is true then
 	tell application "Music"
 		set trackName to name of current track
 		set trackArtist to artist of current track
